8f2dc6ee65a8ee0c7d83878a2256e9ba9cb8f062
miguel Borquez <miguelborquez7@gmail.com>
2025-09-07 20:27:43 -0700

    fields displaying implementation

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -5,14 +5,42 @@ ActiveAdmin.register Project do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :web_developer_account_id, :name, :description, :status
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:web_developer_account_id, :name, :description, :status]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :web_developer_account_id, :plan, :name, :description, :status
+
+
+
+  index do
+    selectable_column
+    id_column
+    column :web_developer_account
+    column :plan
+    column :name
+    column :status
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs "Project Details" do
+      f.input :web_developer_account, as: :select, collection: WebDeveloperAccount.all.map { |a| ["#{a.user.email} (#{a.plan&.name})", a.id] }
+      f.input :plan, as: :select, collection: Plan.all.map { |p| [p.name, p.id] }
+      f.input :name
+      f.input :description
+      f.input :status, as: :select, collection: ["in progress", "completed", "on hold"]
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :web_developer_account
+      row :plan
+      row :name
+      row :description
+      row :status
+      row :created_at
+      row :updated_at
+    end
+  end
 end
+
